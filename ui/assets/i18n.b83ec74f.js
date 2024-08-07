5f3a6a12215408dde84baa6e354ce8d54a6a0f35
刘卓 <liuzhuogood@foxmail.com>
2024-08-07 10:13:50 +0800

    fix(ui): add tip

--- ui/assets/i18n.b83ec74f.js
+++ ui/assets/i18n.b83ec74f.js
@@ -1,4 +1,4 @@
-import{d as dt,h as zt,e as An,i as Sn,o as Pn,a as yn,c as Oe,w as st,F as en,g as tn,b as Rn,r as kn,s as Dn,f as Mn,T as Fn,j as vn}from"./index.b387db58.js";/*!
+import{d as dt,h as zt,e as An,i as Sn,o as Pn,a as yn,c as Oe,w as st,F as en,g as tn,b as Rn,r as kn,s as Dn,f as Mn,T as Fn,j as vn}from"./index.98bb001d.js";/*!
   * shared v9.13.1
   * (c) 2024 kazuya kawaguchi
   * Released under the MIT License.
