93c50567dd7f105a9b0d86b53d6b702f74747db0
刘卓 <liuzhuogood@foxmail.com>
2024-08-09 00:07:30 +0800

    feat(api): 下载优化，预留搜索的信息，用于以后再次使用

--- ui/assets/Favorite.3444a522.js
+++ ui/assets/Favorite.3444a522.js
@@ -1,4 +1,4 @@
-import{Q as Et}from"./QPage.48361e4f.js";import{c as yt,_ as Wt}from"./_commonjsHelpers.dd4c501b.js";import{r as mt,o as Pt,w as _t,Z as wt,aa as At,_ as Rt,$ as zt,f as Ot}from"./index.78b68d71.js";var gt={exports:{}};/*!
+import{Q as Et}from"./QPage.ac31ae3e.js";import{c as yt,_ as Wt}from"./_commonjsHelpers.dd4c501b.js";import{r as mt,o as Pt,w as _t,Z as wt,aa as At,_ as Rt,$ as zt,f as Ot}from"./index.ce965ad9.js";var gt={exports:{}};/*!
  * wordcloud2.js
  * http://timdream.org/wordcloud2.js/
  *
