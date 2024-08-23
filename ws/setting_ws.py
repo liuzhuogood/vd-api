3399997254219d4ef29f5530e8b5c35836faf714
刘卓 <liuzhuogood@foxmail.com>
2024-08-23 15:13:47 +0800

    feat(api) : fix

--- ws/setting_ws.py
+++ ws/setting_ws.py
@@ -4,12 +4,14 @@ import socketio
 
 from common.response import success
 from common.config import Config
+from common.util import Authorization
 
 cwd = os.path.dirname(__file__)
 
 
 class SettingWS(socketio.Namespace):
 
+    @Authorization
     def on_setting(self, sid, data):
         obj = {}
         Config.load()
@@ -18,6 +20,7 @@ class SettingWS(socketio.Namespace):
 
         return success(data=obj)
 
+    @Authorization
     def on_save(self, sid, data):
         Config.save(data)
         return success()
